8808bcb50725106309d4de59889148e8dc6ff0b4
Yang-wenhui <923908075@qq.com>
2025-01-06 22:33:47 +0800

    Update FileOperations.kt
    添加剪贴板复制粘贴操作、文件预览功能、代码优化与测试优化文件搜索和操作的处理逻辑

--- FileOperations.kt
+++ FileOperations.kt
@@ -7,12 +7,12 @@ import java.nio.file.StandardCopyOption;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
-
+//用于复制文件11
 public class FileOperations {
     public static void copyFile(String sourceFile, String destinationFile) throws IOException {
         Files.copy(Paths.get(sourceFile), Paths.get(destinationFile), StandardCopyOption.REPLACE_EXISTING);
     }
-
+//文件预览功能相关代码11
     public static String previewTextFile(String filePath, int numLines) throws IOException {
         StringBuilder preview = new StringBuilder();
         Charset charset = Charset.forName("UTF-8"); // 使用UTF-8编码
@@ -39,7 +39,7 @@ public class FileOperations {
             }
         }
     }
-
+//用于获取文件的扩展名01、11
     public static String getFileExtension(String fileName) {
         int dotIndex = fileName.lastIndexOf(".");
         if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
